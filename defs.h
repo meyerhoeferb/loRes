7917c5d21e7aaed24f49680d8feb99633692cf3f
Braden Meyerhoefer <bredcurve@gmail.com>
2020-07-26 16:54:48 -0400

    Add undo and history structure

--- defs.h
+++ defs.h
@@ -6,6 +6,8 @@ typedef unsigned long long U64;
 #define NAME "loRes 1.0"
 #define BRD_SQ_NUM 120
 
+#define MAX_GAME_MOVES 2048        //max allowed half moves in a game
+
 enum {EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK}; //piecetypes
 enum {FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE}; //file and row translations
 enum {RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE};
@@ -20,7 +22,18 @@ enum {A1 = 21, B1, C1, D1, E1, F1, G1, H1,
       A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ        //chess coord to array index
 };
 
-enum{FALSE, TRUE};      //boolean
+enum {FALSE, TRUE};      //boolean
+
+enum {WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8};          //set bits for castling permissions
+
+//undo move structure
+typedef struct{
+    int move;       //move played
+    int castlePerm; //permissions before move
+    int enPas;      //enpas before move
+    int fiftyMove;  //count before move
+    U64 posKey;     //hash of position prior to move
+} S_UNDO;
 
 //board structure
 typedef struct {
@@ -36,6 +49,8 @@ typedef struct {
     int ply;                    //number of halfmoves in current search
     int hisPly;                 //number of halfmoves in total game so far
 
+    int castlePerm;             //castling permissions
+
     U64 posKey;                 //hash of position (for repitition)
 
     int pcNum[13];              //count of pieces on board
@@ -43,7 +58,8 @@ typedef struct {
     int majPc[3];               //count of major pieces on board
     int minPc[3];               //count of minor pieces on board
 
-    
+    S_UNDO history[MAX_GAME_MOVES]; //history of game positions/moves 
+
 } S_BOARD;
 
 #endif
