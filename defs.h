606716ed93d56d8268d20b9799caeb0f832b84fe
Braden Meyerhoefer <bredcurve@gmail.com>
2020-07-27 20:06:13 -0400

    Add code for parsing fen notation.

--- defs.h
+++ defs.h
@@ -24,8 +24,10 @@ typedef unsigned long long U64;
 
 #define MAX_GAME_MOVES 2048        //max allowed half moves in a game
 
+#define START_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
 enum {EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK}; //piecetypes
-enum {FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE}; //file and row translations
+enum {FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE};  //file and row translations
 enum {RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE};
 enum {WHITE, BLACK, BOTH};  //piece colors
 enum {A1 = 21, B1, C1, D1, E1, F1, G1, H1,
@@ -35,7 +37,7 @@ enum {A1 = 21, B1, C1, D1, E1, F1, G1, H1,
       A5 = 61, B5, C5, D5, E5, F5, G5, H5,
       A6 = 76, B6, C6, D6, E6, F6, G6, H6,
       A7 = 81, B7, C7, D7, E7, F7, G7, H7,
-      A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ        //chess coord to array index
+      A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ        //chess coord to array index 
 };
 
 enum {FALSE, TRUE};      //boolean
